66ab1430051fd2eb22ccdf7221c7b962182ab4e2
Jared Henry <henryjc@f8ffc257ee95.local>
2023-01-11 17:21:02 -0600

    updated

---
+++ README.md
@@ -0,0 +1,3 @@
+```
+generator -path=./yang/vendor/cisco/xr/732 -generate_fakeroot -fakeroot_name=interfaces -output_file=pkg/interfaces/interfaces.go -package_name=cisco Cisco-IOS-XR-ifmgr-cfg.yang
+```
---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"github.com/openconfig/ygot/ygot"
+	"yangstuff/pkg/interfaces"
+)
+
+func main() {
+	i := interfaces.Interfaces{}
+	i.InterfaceConfigurations = &interfaces.Cisco_IOS_XRIfmgrCfg_InterfaceConfigurations{}
+	ni, err := i.InterfaceConfigurations.NewInterfaceConfiguration("act", "Loopback333")
+	if err != nil {
+		fmt.Println(err)
+	}
+	ni.Description = ygot.String("Wow:This:Worked")
+	bs, err := ygot.Marshal7951(ni)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(bs))
+
+	json, err := ygot.EmitJSON(ni, &ygot.EmitJSONConfig{
+		Format: ygot.RFC7951,
+		Indent: "  ",
+		RFC7951Config: &ygot.RFC7951JSONConfig{
+			AppendModuleName: false,
+		},
+	})
+	fmt.Println(json)
+}
