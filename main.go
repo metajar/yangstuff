82734313c1fc0b4e4602a1d15c99a807af691e80
Jared Henry <henryjc@f8ffc257ee95.local>
2023-01-11 20:58:42 -0600

    added mpls

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 )
 
 func main() {
+
 	i := interfaces.Interfaces{}
 	i.InterfaceConfigurations = &interfaces.Cisco_IOS_XRIfmgrCfg_InterfaceConfigurations{}
 	ni, err := i.InterfaceConfigurations.NewInterfaceConfiguration("act", "Loopback333")
@@ -14,17 +15,18 @@ func main() {
 		fmt.Println(err)
 	}
 	ni.Description = ygot.String("Wow:This:Worked")
+	ni.Shutdown = true
 	bs, err := ygot.Marshal7951(ni)
 	if err != nil {
 		fmt.Println(err)
 	}
 	fmt.Println(string(bs))
 
-	json, err := ygot.EmitJSON(ni, &ygot.EmitJSONConfig{
+	json, err := ygot.EmitJSON(&i, &ygot.EmitJSONConfig{
 		Format: ygot.RFC7951,
 		Indent: "  ",
 		RFC7951Config: &ygot.RFC7951JSONConfig{
-			AppendModuleName: false,
+			AppendModuleName: true,
 		},
 	})
 	fmt.Println(json)
