4212bd8af4795b855cadfeadbf9a1d191c63722b
Jared Henry <henryjc@f8ffc257ee95.local>
2023-01-11 21:35:59 -0600

    added a quick delete of mpls tunnel

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module yangstuff
 go 1.19
 
 require (
+	github.com/nleiva/xrgrpc v0.6.0
 	github.com/openconfig/goyang v1.2.0
 	github.com/openconfig/ygot v0.25.6
 )
@@ -13,10 +14,10 @@ require (
 	github.com/google/go-cmp v0.5.7 // indirect
 	github.com/kylelemons/godebug v1.1.0 // indirect
 	github.com/openconfig/gnmi v0.0.0-20220920173703-480bf53a74d2 // indirect
-	golang.org/x/net v0.0.0-20210805182204-aaa1db679c0d // indirect
-	golang.org/x/sys v0.0.0-20210809222454-d867a43fc93e // indirect
+	golang.org/x/net v0.0.0-20220425223048-2871e0cb64e4 // indirect
+	golang.org/x/sys v0.0.0-20220503163025-988cb79eb6c6 // indirect
 	golang.org/x/text v0.3.7 // indirect
-	google.golang.org/genproto v0.0.0-20210811021853-ddbe55d93216 // indirect
-	google.golang.org/grpc v1.40.0 // indirect
+	google.golang.org/genproto v0.0.0-20220505152158-f39f71e6c8f3 // indirect
+	google.golang.org/grpc v1.46.0 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,12 +2,37 @@ package main
 
 import (
 	"fmt"
+	xr "github.com/nleiva/xrgrpc"
 	"github.com/openconfig/ygot/ygot"
+	"log"
 	"yangstuff/pkg/interfaces"
 )
 
 func main() {
+	router, err := xr.BuildRouter(
+		xr.WithUsername("grpc"),
+		xr.WithPassword("53cret"),
+		xr.WithHost("192.168.88.3:57344"),
+		xr.WithTimeout(50),
+	)
 
+	conn, ctx, err := xr.Connect(*router)
+	if err != nil {
+		log.Fatalf("could not setup a client connection to %s, %v", router.Host, err)
+	}
+	defer conn.Close()
+	var output string
+
+	var yp = `{	
+    "Cisco-IOS-XR-ifmgr-cfg:interface-configurations": [null]
+}
+`
+
+	output, err = xr.GetConfig(ctx, conn, yp, 1001)
+	if err != nil {
+		log.Fatalf("could not get the config from %s, %v", router.Host, err)
+	}
+	fmt.Printf("\nconfig from %s\n %s\n", router.Host, output)
 	i := interfaces.Interfaces{}
 	i.InterfaceConfigurations = &interfaces.Cisco_IOS_XRIfmgrCfg_InterfaceConfigurations{}
 	ni, err := i.InterfaceConfigurations.NewInterfaceConfiguration("act", "Loopback333")
@@ -15,7 +40,6 @@ func main() {
 		fmt.Println(err)
 	}
 	ni.Description = ygot.String("Wow:This:Worked")
-	ni.Shutdown = true
 	bs, err := ygot.Marshal7951(ni)
 	if err != nil {
 		fmt.Println(err)
@@ -30,4 +54,30 @@ func main() {
 		},
 	})
 	fmt.Println(json)
+
+	io, err := xr.MergeConfig(ctx, conn, json, 100023)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(io)
+	xr.CommitConfig(ctx, conn, uint32(io), 3333)
+
+	deleteLspInterfaces := interfaces.Interfaces{}
+	deleteLspInterfaces.InterfaceConfigurations = &interfaces.Cisco_IOS_XRIfmgrCfg_InterfaceConfigurations{}
+	_, err = deleteLspInterfaces.InterfaceConfigurations.NewInterfaceConfiguration("act", "tunnel-te29")
+	if err != nil {
+		fmt.Println(err)
+	}
+	json, err = ygot.EmitJSON(&deleteLspInterfaces, &ygot.EmitJSONConfig{
+		Format: ygot.RFC7951,
+		Indent: "  ",
+		RFC7951Config: &ygot.RFC7951JSONConfig{
+			AppendModuleName: true,
+		},
+	})
+	print(json)
+	io, err = xr.DeleteConfig(ctx, conn, json, 100023333)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
